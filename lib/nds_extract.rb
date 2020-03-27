7a6decf70ceb752ab35a251da499d792d30d5c34
Elias Taveras <eliastave12@gmail.com>
2020-03-27 04:25:12 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,43 +1,44 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
-require_relative './directors_database'
-
-def directors_totals(source)
-  result = {}
-  director_index = 0
-  while director_index < source.size do
-    director = source[director_index]
-    result[director[:name]] = gross_for_director(director)
-    director_index += 1
-  end
-  result
-end
-
-def gross_for_director(d)
-  total = 0
-  index = 0
-
-  while index < d[:movies].length do
-    total += d[:movies][index][:worldwide_gross]
-    index += 1
-  end
-
-  total
-end
+require 'directors_database'
 
 def list_of_directors(source)
-  # Write this implementation
+  director_list=[]
+  counter= 0
+  while counter<source.length
+  director_list << source[counter][:name]
+  counter +=1
+  end
+  director_list
 end
 
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+    counter = 0
+    grand_total = 0
+    director_total = directors_totals(source) 
+    director_list = list_of_directors(source)
+    while counter < director_list.length do
+      grand_total += director_total[director_list[counter]]
+      counter += 1
+    end
+    grand_total
+  end
+
+def gross_for_director(director_data)
+  integer=0
+  counter = 0
+  while counter < director_data[:movies].length do
+  integer += director_data[:movies][counter][:worldwide_gross]
+  counter += 1
+  end
+ integer
 end
-
-
+ 
+def directors_totals(nds)
+  hash = {}
+  director_number = 0
+  while director_number < nds.length do
+    hash[nds[director_number][:name]] = gross_for_director(directors_database[director_number])
+    director_number += 1
+  end
+ hash
+end
